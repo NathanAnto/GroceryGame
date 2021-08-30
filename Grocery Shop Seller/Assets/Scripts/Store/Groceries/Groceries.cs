37890cdf5ec6c00823a1c1213e96eb994720d205
Nathan Antonietti <47625153+NathanAnto@users.noreply.github.com>
2021-08-30 22:53:43 +0200

    Revert "Feature shopper assistance"

--- Grocery Shop Seller/Assets/Scripts/Store/Groceries/Groceries.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Groceries
-{
-    // groceries is case sensitive and must match the string "groceries" in the JSON.
-    public Grocery[] groceries;
-}
